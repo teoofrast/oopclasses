4339172a69f7395cd63fedebc7cb0ac574471888
teoofrast <teoofrast@gmail.com>
2023-12-15 00:13:47 +0600

    add init to second file

--- oopclasses/getters_and_setters.py
+++ oopclasses/getters_and_setters.py
@@ -1,5 +1,9 @@
 class MeansOfTransport:
 
+    def __init__(self, color, brand):
+        self.__color = color
+        self.__brand = brand
+
     def set_color(self, color):
         self.__color = color
 
@@ -12,6 +16,6 @@ class MeansOfTransport:
     def get_brand(self):
         return self.__brand
 
-trans1 = MeansOfTransport(input("input a color of vehicle: "), input("input a brand of vehicle: "))
+trans1 = MeansOfTransport()
 trans1.set_brand(input("Input a new brand of vehicle: "))
 print(trans1.__dict__)
