cb7cbf0cee564642b7bac568b0d88f20eb1c7a33
teoofrast <teoofrast@gmail.com>
2023-12-15 00:03:39 +0600

    add first class

--- oopclasses/classes.py
+++ oopclasses/classes.py
@@ -1,2 +1,15 @@
-print("hello world")
-print("Fuck you, man")
+class MeansOfTransport:
+
+    def __init__(self, color, brand):
+        self.color = color
+        self.brand = brand
+
+    def show_color(self):
+        return self.color
+
+    def show_brand(self):
+        return self.brand
+
+
+trans1 = MeansOfTransport(input(), input())
+print(trans1.show_brand(), trans1.show_color())
